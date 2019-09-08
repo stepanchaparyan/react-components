22781f0c47591edc42fd479c3a07f6b3482398b0
stepanchaparyan <stepanchaparyan@gmail.com>
2019-09-09 00:59:44 +0400

    update Spinner

--- src/spinner/Spinner.jsx
+++ src/spinner/Spinner.jsx
@@ -1,10 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { PulseLoader, BeatLoader, RotateLoader } from 'react-spinners';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-class Spinner extends Component {
-    static propTypes = {
+const Spinner = ({
+    loaderType, className, size, color, margin
+}) => {
+
+    const classes = classNames(
+        className
+    );
+
+    let Loader = PulseLoader;
+
+    if (loaderType == 'BeatLoader') {
+        Loader = BeatLoader;
+    } else if (loaderType == 'RotateLoader') {
+        Loader = RotateLoader;
+    }
+
+    return (
+        <div className={classes}>
+            <Loader               
+                sizeUnit={'px'}
+                size={size}
+                margin={margin}
+                color={color}
+            />
+        </div>
+    );
+  }
+
+    Spinner.propTypes = {
         className: PropTypes.string,
         size: PropTypes.number,
         color: PropTypes.string,
@@ -12,43 +39,14 @@ class Spinner extends Component {
         loaderType: PropTypes.oneOfType([
             PropTypes.string,
             PropTypes.func
-          ]),
-    }
-    
-    static defaultProps = {
+            ]),
+        };
+        
+        Spinner.defaultProps = {
         color: '#17a2b8',
         margin: '5px',
         size: 20,
         loaderType: PulseLoader
-    }
-
-    render() {
-        const { loaderType, className, size, color, margin } = this.props;
-        
-        const classes = classNames(
-            'spinner',
-            className
-        );
-
-        let Loader = PulseLoader;
-
-        if (loaderType == 'BeatLoader') {
-            Loader = BeatLoader;
-        } else if (loaderType == 'RotateLoader') {
-            Loader = RotateLoader;
-        }
-
-        return (
-            <div className={classes}>
-                <Loader               
-                    sizeUnit={'px'}
-                    size={size}
-                    margin={margin}
-                    color={color}
-                />
-            </div>
-        );
-    }
-}
+        };
 
 export default Spinner;
